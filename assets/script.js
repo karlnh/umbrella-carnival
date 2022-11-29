4446b64634ab7b60edcf83bc0bedaab715f857a5
karlnh <hubigkarl@gmail.com>
2022-11-29 00:56:53 -0600

    write js todo list

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const APIKey = "";
+// https://home.openweathermap.org/api_keys
+
+// TODO: Write a function which grabs a selected city and its weather information through OWM's API.
+    // Write a subfunction which concats the newest search to localStorage along with a clickable link to the city weather data. Only five most recent searched cities should be displayed. This may involve simply cutting out the first element of the localStorage array.
+
+// TODO: Display past searches in the Previous searches section by grabbing it from localStorage.
+
+// TODO: Write a function which grabs the current date and shows it in the Current Weather header.
+
+// TODO: Write a function which grabs current temperature, humidity, and wind speed for a given city;
+    // Display city name;
+    // Associate types of weather with different emojis, and display the correct emoji next to the city name;
+    // Display temp, humidity, and wind speed information in the corresponding element.
+
+// TODO: Write a function which generates HTML level-items for the 5-Day Forecast.
+    // Grab 5-Day Forecast information starting from the current day.
+    // Display temp, humidity, and wind speed information for each day.
+
