d094ff01d386dd8f1a3f738c608d2e856e8f1c69
karlnh <hubigkarl@gmail.com>
2022-12-03 03:15:33 -0600

    add current date; grab weather of selected city

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,30 @@
 const APIKey = "43dd332c7883656c9b184a8861ced36a";
 // https://home.openweathermap.org/api_keys
 
+const searchLimit = 5;
+
+const dateSpan = $('#date');
 const searchBtn = $('#search-button');
 const searchTextEl = $('.input');
 const cityModal = $('#modal-city-select');
-const cityModalList = $('city-list');
+const cityList = $('#city-list');
+const weatherNow = $('#weather-now-vars');
+const selectedCity = $('#selected-city');
+const cityWeatherIcon = $('#selected-city-weather-icon');
 
-const searchLimit = 5;
-
-let cityList = $('#city-list');
-let citySearch
-let cityQuery = "http://api.openweathermap.org/geo/1.0/direct?q="+citySearch+"&limit=5&appid="+APIKey;
+let citySearch;
 let cityCoords = "";
+let weatherList = weatherNow[0].children;
 
 function main() {
+    dateSpan.text(dayjs().format("ddd, MMM D"));
+    setInterval(function () {
+        dateSpan.text(dayjs().format("ddd, MMM D"));
+    }, 3600000); // updates every hour
+    
+    
     // On click, show modal populated with possible cities.
+
     searchBtn.click(function(event) {
         cityList.empty(); // clear modal list on new search
         let search = searchTextEl[0].value;
@@ -24,40 +34,60 @@ function main() {
                 cityModal.removeClass('is-active');
             });
             fetch("https://api.openweathermap.org/geo/1.0/direct?q="+search+"&limit="+searchLimit+"&appid="+APIKey
-            ).then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                // iterating through the data to get cities
-                for (let i = 0; i < data.length; i++) {
-                    let city = data[i];
-                    if (city.state !== undefined) { // if its a US state
-                        console.log(city.name, city.state);
-                        cityList.append("<li " +
-                        "data-lat=" + data[i].lat + " data-lon=" + data[i].lon + ">" +
-                        city.name + ", " + city.state + ", " + city.country +
-                        "</li>");
-                        // VSCode is suggesting to turn the previous into the following:
-                        // `<li lat=${data[i].lat} lon=${data[i].lon}>${city.name}, ${city.state}, ${city.country}</li>`
-                    } else { // if not a US state
-                        console.log(city.name, city.country);
-                        cityList.append("<li " +
-                        "data-lat=" + data[i].lat + " data-lon=" + data[i].lon + ">" +
-                        city.name + ", " + city.country +
-                        "</li>"
-                        );
-                        // VSCode is suggesting to turn the previous into the following:
-                        // `<li lat=${data[i].lat} lon=${data[i].lon}>${city.name}, ${city.country}</li>`
+            )
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    // iterating through the data to get cities
+                    for (let i = 0; i < data.length; i++) {
+                        let city = data[i];
+                        if (city.state !== undefined) { // if its a US state
+                            // console.log(city.name, city.state);
+                            cityList.append("<li " +
+                            "data-city-name=" + city.name +
+                            " data-lat=" + data[i].lat + " data-lon=" + data[i].lon + ">"
+                            + city.name + ", " + city.state + ", " + city.country +
+                            "</li>");
+                            // VSCode is suggesting to turn the previous into the following:
+                            // `<li lat=${data[i].lat} lon=${data[i].lon}>${city.name}, ${city.state}, ${city.country}</li>`
+                        } else { // if not a US state
+                            // console.log(city.name, city.country);
+                            cityList.append("<li " +
+                            "data-city-name=" + city.name +
+                            " data-lat=" + data[i].lat + " data-lon=" + data[i].lon + ">"
+                            + city.name + ", " + city.country +
+                            "</li>"
+                            );
+                            // VSCode is suggesting to turn the previous into the following:
+                            // `<li lat=${data[i].lat} lon=${data[i].lon}>${city.name}, ${city.country}</li>`
+                        }
                     }
-                }
-            });
-
+                });
+        // grabs weather information about clicked city.
         cityList.on('click', function (event) {
-            let element = event.target;
-            if (element.matches("li")) {
-                console.log(element);
-                cityLat = element.lat;
-                cityLon = element.lon;
 
+            let element = event.target;
+            if (element.matches("li")) { // if we're actually clicking the list element
+                cityModal.removeClass('is-active'); // hides modal
+                let cityName = $(element).data('city-name');
+                let cityLat = $(element).data('lat');
+                let cityLon = $(element).data('lon');
+                fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+cityLat+"&lon="+cityLon+"&appid="+APIKey)
+                    .then(function (response) { return response.json();
+                    })
+                    .then(function (data) {
+                        // console.log(data);
+                        let currentWeather = data.list[0];
+                        selectedCity.text(cityName);
+                        cityWeatherIcon.text(currentWeather.weather.main); // associate the icon with the weather.
+                        // TODO: get these to be the right measurement.
+                        weatherList[0].textContent = currentWeather.main.temp;
+                        weatherList[1].textContent = currentWeather.main.humidity;
+                        weatherList[2].textContent = currentWeather.wind.speed;
+                        
+
+                    });
             }
         })
 
@@ -69,18 +99,9 @@ function main() {
 
 
 }
-
-    // TODO: Write a function which grabs a selected city and its weather information through OWM's API.
     // Write a subfunction which concats the newest search to localStorage along with a clickable link to the city weather data. Only five most recent searched cities should be displayed. This may involve simply cutting out the first element of the localStorage array.
 
     // TODO: Display past searches in the Previous searches section by grabbing it from localStorage.
-
-    // TODO: Write a function which grabs the current date and shows it in the Current Weather header.
-
-    // TODO: Write a function which grabs current temperature, humidity, and wind speed for a given city;
-        // Display city name;
-        // Associate types of weather with different emojis, and display the correct emoji next to the city name;
-        // Display temp, humidity, and wind speed information in the corresponding element.
 
     // TODO: Write a function which generates HTML level-items for the 5-Day Forecast.
         // Grab 5-Day Forecast information starting from the current day.
